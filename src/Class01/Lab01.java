5fbdd807d906b896c4b8e60efded3529edf36a1b
Mohammad Jakaria <mohammadjakaria@MohammdkariaMBP.home>
2020-09-15 02:42:40 -0400

    First homework

--- src/Class01/Lab01.java
+++ src/Class01/Lab01.java
@@ -1,4 +1,42 @@
 package Class01;
 
 public class Lab01 {
+    public static void main(String[] args) {
+        System.out.println("Good Night");
+    byte noOfHours = 24;
+    System.out.println("No of hours in a day " + noOfHours);
+
+    int maxNoOfDays = 365;
+        System.out.println("Max no. of days in an a year " +  maxNoOfDays);
+
+    int noOfEmployees = 1000;
+        System.out.println("Total number of employees in an organization is " + noOfEmployees);
+
+    long populationInCountry = 99999999L;
+        System.out.println("Population in a Country is " + populationInCountry);
+
+        float intRate = 2.5f;
+        System.out.println("Interest rate is " + intRate);
+
+    double balanceInBankAc = 10000;
+        System.out.println("My bank balance is "+ balanceInBankAc + " Dollar");
+    Boolean sunRiseWhichDirection = true;
+        System.out.println("Does the sun rise in the west ? " + sunRiseWhichDirection );
+
+
+        char initalOfMyName1= 'M';
+        char initalOfMyName2= 'J';
+        System.out.println("Initial of my name is " + initalOfMyName1+initalOfMyName2);
+
+      String myName = "Mohammad Jakaria";
+        System.out.println("My Name is " + myName);
+
+
+
+
+
+
+    }
+
 }
+
